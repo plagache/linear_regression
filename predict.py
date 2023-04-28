ffb3c98082557e31f34dd8fe13bb061d4e500fb3
plgch <plagache@protonmail.com>
2023-04-28 13:55:31 +0200

    we clean the predict program;

--- predict.py
+++ predict.py
@@ -3,11 +3,17 @@
 import pandas  as pd
 
 theta = pd.read_csv('theta.csv')
+
 theta_f = pd.DataFrame(theta)
+
+
 theta_0 = theta_f.loc[theta_f.index[-1], 'theta0']
-theta_1 = theta_f.loc[theta_f.index[-1], "theta1"]
-print(theta_0)
+theta_1 = theta_f.loc[theta_f.index[-1], 'theta1']
+
+print("Theta 0 = ", theta_0, "\nTheta 1 = ", theta_1, sep='', end='\n')
 
 km = input("Enter your mileage: ")
 
-print(theta_0[0] +( theta_1[0] * float(km)) )
+estimate_Price = theta_0 + ( theta_1 * float(km))
+
+print(estimate_Price)
